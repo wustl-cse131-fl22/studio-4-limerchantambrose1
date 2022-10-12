2e4b958ecb62e94de91740e5da5f3e931e154c91
eileenli <eileenli@eileens-mbp.dhcp.wustl.edu>
2022-10-12 15:35:10 -0500

    fftt

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,57 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+	
+	
+		
+		double x1 = .25;
+		double y1 = .125;
+		double halfHeight = .125;
+		double halfWidth = .25;
+		
+		
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle( x1,  y1, halfWidth, halfHeight);
+		
+		
+		double x2 = .75;
+		double y2 = .125;
+		
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle( x2,  y2, halfWidth, halfHeight);
+		
+		
+		double x3 = .25;
+		double y3 = .375;
+		
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle( x3,  y3, halfWidth, halfHeight);
+		
+		double x4 = .75;
+		double y4 = .375;
+		
+		
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle( x4,  y4, halfWidth, halfHeight);
+		
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledCircle(.25, .375, .1);
+		
+		StdDraw.filledCircle(.75, .125, .1);
+
+		
+		
+		
+		
+		StdDraw. setPenRadius(.025);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(.5, .25, .5, .25);
+	
+		
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,47 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();  
+		//System.out.print(shape);
+		
+		int red = in.nextInt(); 
+		//System.out.print(red);
+		
+		int green = in.nextInt(); 
+		//System.out.print(green);
+		
+		int blue = in.nextInt(); 
+		//System.out.print(blue);
+		
+		boolean isFilled= in.nextBoolean(); 
+		
+		double x = in.nextDouble(); 
+		double y = in.nextDouble(); 
+		double halfW = in.nextDouble(); 
+		double halfH = in.nextDouble(); 
+		
+		StdDraw.setPenColor(red, green, blue);
+		if (shape.equals("ellipse")) {
+			if (!isFilled)
+			{
+				StdDraw.ellipse( x,  y, halfW, halfH);
+			}else {
+				StdDraw.filledEllipse( x,  y, halfW, halfH);
+			}
+		}
+		else if (shape.equals("rectangle")) {
+			if (!isFilled)
+			{
+				StdDraw.rectangle( x,  y, halfW, halfH);
+			}else {
+				StdDraw.filledRectangle( x,  y, halfW, halfH);
+			}
+		}
+		
+		
+		
+		
+		
+		
 	}
 }
